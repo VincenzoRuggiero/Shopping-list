10d6e4386e86905670f521c7a207796bbf97386a
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-09-02 12:03:41 +0200

    Handle empty field input

--- app.js
+++ app.js
@@ -22,10 +22,13 @@ const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+  if (!inputValue) {
+    shoppingListEl.innerHTML = `<p style="color: red; margin: 0; font-weight: bold;">Per favore inserisci un prodotto!</p>`;
+  } else {
+    push(shoppingListInDB, inputValue);
 
-  push(shoppingListInDB, inputValue);
-
-  clearInputFieldEl();
+    clearInputFieldEl();
+  }
 });
 
 onValue(shoppingListInDB, function (snapshot) {
